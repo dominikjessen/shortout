4e33abcadec177d05c065f314c701d3009abebc4
Dominik <djessen94@gmail.com>
2023-04-19 12:02:30 +0200

    Can remove shorts section from Home

--- src/background.ts
+++ src/background.ts
@@ -8,16 +8,28 @@ function hideShortsFromSubscriptionsPage(): void {
   shortsGridElements.forEach((short) => short?.remove());
 }
 
+function hideShortsSectionFromHomePage(): void {
+  const shortsSectionElement = document.querySelector('ytd-rich-shelf-renderer[is-shorts]')?.closest('ytd-rich-section-renderer');
+  shortsSectionElement?.remove();
+}
+
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url !== 'https://www.youtube.com/feed/subscriptions') {
+  let funcToCall = function () {};
+  if (!tab.url?.includes('youtube.com')) {
     console.log(`This isn't YouTube, dummy!`);
     return;
+  }
+
+  if (tab.url.includes('subscriptions')) {
+    funcToCall = hideShortsFromSubscriptionsPage;
+  } else if (tab.url === 'https://www.youtube.com/') {
+    funcToCall = hideShortsSectionFromHomePage;
   }
 
   chrome.scripting
     .executeScript({
       target: { tabId: tab.id ? tab.id : -1 },
-      func: hideShortsFromSubscriptionsPage
+      func: funcToCall
     })
     .then();
 });
