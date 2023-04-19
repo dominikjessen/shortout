a43aa7b8a4b77dcd00585e38788e509d200e638b
Dominik <djessen94@gmail.com>
2023-04-19 11:25:08 +0200

    On click, removes shorts on page from subs

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,23 @@
-function changeBgColor(color: string): void {
-  document.body.style.backgroundColor = color;
+function hideShortsFromSubscriptionsPage(): void {
+  // Note this querySelector could break if YT changes their page structure
+  const shortsGridElements = [...document.querySelectorAll('ytd-thumbnail-overlay-time-status-renderer[overlay-style="SHORTS"]')].map((element) =>
+    element.closest('ytd-grid-video-renderer')
+  );
+
+  // TODO: In the future I could do something with these shorts, like saving them to a bin in the extension
+  shortsGridElements.forEach((short) => short?.remove());
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  const color = 'blue';
+  if (tab.url !== 'https://www.youtube.com/feed/subscriptions') {
+    console.log(`This isn't YouTube, dummy!`);
+    return;
+  }
+
   chrome.scripting
     .executeScript({
       target: { tabId: tab.id ? tab.id : -1 },
-      func: changeBgColor,
-      args: [color]
+      func: hideShortsFromSubscriptionsPage
     })
     .then();
 });
