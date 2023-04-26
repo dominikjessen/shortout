652d56aefdbc0e2a820155084a0b4f6a6a2a156a
Dominik <djessen94@gmail.com>
2023-04-26 15:12:51 +0200

    Delete a single channel from whitelist

--- src/popup.ts
+++ src/popup.ts
@@ -5,7 +5,7 @@ function createWhitelistBaseElement() {
 
 // Helper function to make style changes easy
 function liHtmlString(text: string): string {
-  return `<li class="py-2 px-3 hover:bg-rose-100 rounded flex text-base"><img src="/icons/play.svg" alt="play button" class="" /><span class="text-neutral-800 px-3 grow cursor-default">${text}</span><span class="text-bold text-rose-400">YY</span></li>`;
+  return `<li class="py-2 px-3 hover:bg-rose-100 rounded flex text-base"><img src="/icons/play.svg" alt="play button" class="" /><span class="text-neutral-800 px-3 grow cursor-default">${text}</span><button class="delete-whitelist-entry hover:fill-blue-500"><img src="/icons/trash.svg" alt="delete button" /></button></li>`;
 }
 
 async function constructWhitelistElements() {
@@ -50,8 +50,27 @@ async function clearWhitelist() {
   document.getElementById('whitelist')?.remove();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  constructWhitelistElements();
+async function removeWhitelistItem(event: Event) {
+  const target = event.target as HTMLElement;
+  const li = target.closest('li');
+  const channelToDelete = li?.innerText;
+
+  if (!channelToDelete) return;
+
+  // Update chrome storage
+  const currentWhitelist = await getStoredWhitelistChannels();
+  const index = currentWhitelist.indexOf(channelToDelete);
+  if (index !== -1) {
+    currentWhitelist.splice(index, 1);
+  }
+  await chrome.storage.local.set({ whitelist: currentWhitelist });
+
+  // Remove li
+  li.remove();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await constructWhitelistElements();
 
   const closeBtn = document.getElementById('popup-close');
   closeBtn?.addEventListener('click', () => window.close());
@@ -61,4 +80,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const clearWhitelistBtn = document.getElementById('clear-whitelist-button');
   clearWhitelistBtn?.addEventListener('click', clearWhitelist);
+
+  const deleteButtons = [...document.querySelectorAll('.delete-whitelist-entry')];
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener('click', removeWhitelistItem);
+  });
 });
