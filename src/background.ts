12ec603661b964f79dc4b66963f9f3d158b3d621
Dominik <djessen94@gmail.com>
2023-05-02 22:11:56 +0200

    Init chrome.storage.local

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,14 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  // NOTE: not sure if this should be somewhere else for perf?
+  // Init store
+  const chromeLocalStore = await chrome.storage.local.get(null);
+  if (chromeLocalStore.whitelist === null || chromeLocalStore.whitelist === undefined) {
+    await chrome.storage.local.set({ whitelist: [] });
+  }
+  if (chromeLocalStore.tabExtensionActiveStates === null || chromeLocalStore.tabExtensionActiveStates === undefined) {
+    await chrome.storage.local.set({ tabExtensionActiveStates: {} });
+  }
+
   if (changeInfo.status == 'complete' && tab.active && tab.url?.includes('youtube.com')) {
     let pageType;
     if (tab.url?.includes('subscriptions')) {
